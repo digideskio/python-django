76cd212e60994fe180034b042f4aa0a258e016ee
site-demo <site9demo@monumentmail.com>
2015-06-22 15:37:58 -0500

    updating Blog Post Template with data from Post Model using a QuerySet in the Blog View

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post  				# . means current directory or current application
 
 # Blog Views
 
+'''
+	Views are supposed to do: connect models and templates. In our post_list view we will need
+	to take models we want to display and pass them to the template. So basically in a view we 
+	decide what (model) will be displayed in a template.
+
+'''
+
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts' : posts})
 
