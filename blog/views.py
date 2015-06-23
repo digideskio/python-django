fd721003ea8605b50c2b9b8ce26d714bcb4f27e7
site-demo <site9demo@monumentmail.com>
2015-06-22 21:36:18 -0500

    checking for empty blog.

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ from django.shortcuts import redirect
 '''
 
 def post_list(request):
-	if Post.objects.exists(): 
+	if Post.exists(): 
 		posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 		return render(request, 'blog/post_list.html', {'posts' : posts})
 	else:
