5cd9adf5f9bbef92cc3462252b0fd61ca6d45c05
site-demo <site9demo@monumentmail.com>
2015-06-22 20:29:53 -0500

    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}

--- demosite/settings.py
+++ demosite/settings.py
@@ -106,15 +106,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # NOTE:  ADDING BELOW FOR PRODUCTION/DEPLOYMENT SETTINGS
 
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
 DEBUG = True
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+#try:
+#    from .local_settings import *
+#except ImportError:
+ #   pass
